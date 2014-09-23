57ebe6f0347149d540da0fd3818e41e93602d36a
Alf <alf.g.jr@gmail.com>
2014-09-22 19:42:28 -0700

    configure separate environments for client ids

---
+++ js/chathub.js
@@ -0,0 +1,41 @@
+// Globals
+var user;
+var room = window.location.hash.substr(1);
+var host = window.location.host;
+var GITHUB_CLIENT_ID = host == "chathub.github.io" ? "5272ed33c7189d449ea8" : "45d29134d70365fbe5af";
+var SOCKETIO_HOST = host == "chathub.github.io" ? "http://chathub-server.alfg.co" : "http://alf-macbook.local:3000";
+var OAUTH_PROXY_URL = 'https://auth-server.herokuapp.com/proxy';
+
+// Socket.io
+var socket = io(SOCKETIO_HOST);
+$('form').submit(function(){
+  socket.emit('message', room, user + ": " + $('#m').val());
+
+  $('#m').val('');
+  return false;
+});
+
+socket.on('connect', function() {
+   socket.emit('room', room);
+});
+
+socket.on('message', function(msg){
+  $('#messages').append($('<li>').text(msg));
+   console.log('Incoming message:', msg);
+});
+
+// Hello.js
+hello.on('auth.login', function(r){
+  // Get Profile
+  console.log("auth.login");
+  hello.api(r.network+':/me', function(p){
+    var tmpl = "<img src='"+ p.thumbnail + "' width=20/> " +
+    "<strong>" + p.name + "</strong>";
+    $("#github").html(tmpl);
+    user = p.name;
+  });
+});
+
+hello.init({
+  github : GITHUB_CLIENT_ID
+},	{redirect_uri:'../redirect.html', oauth_proxy:OAUTH_PROXY_URL});
