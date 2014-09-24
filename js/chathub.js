79762980139c2e77ecf77c6f71246877f6e9f286
Alf <alf.g.jr@gmail.com>
2014-09-23 21:07:02 -0700

    emoticons and user enter/leave emits

--- js/chathub.js
+++ js/chathub.js
@@ -1,40 +1,59 @@
 // Globals
-var user = "Anonymous ";
+var user = "Anonymous";
 var profile;
 var host = window.location.host;
 var room = window.location.pathname.substr(1) || "Lobby";
 var GITHUB_CLIENT_ID = host == "chathub.github.io" ? "5272ed33c7189d449ea8" : "45d29134d70365fbe5af";
 var SOCKETIO_HOST = host == "chathub.github.io" ? "http://chathub-server.alfg.co" : "http://alf-macbook.local:3000";
-var OAUTH_PROXY_URL = 'https://auth-server.herokuapp.com/proxy';
+var OAUTH_PROXY_URL = "https://auth-server.herokuapp.com/proxy";
 
 // jQuery
 $(document).ready(function() {
   $("[rel='tooltip']").tooltip({"placement": "bottom", "trigger": "hover", "container": "body"});
   $("#room").text(room.replace("/", " / "));
+  if (room != "Lobby") { $("#room").attr("href", "https://github.com/" + room); }
 
+  emojify.setConfig({
+    emojify_tag_type : 'div',           // Only run emojify.js on this element
+    only_crawl_id    : null,            // Use to restrict where emojify.js applies
+    img_dir          : '/images/emoji',  // Directory for emoji images
+    ignored_tags     : {                // Ignore the following tags
+        'SCRIPT'  : 1,
+        'TEXTAREA': 1,
+        'A'       : 1,
+        'PRE'     : 1,
+        'CODE'    : 1
+    }
+  });
+  emojify.run();
 });
 
 // Socket.io
 var socket = io(SOCKETIO_HOST);
-$('form').submit(function(){
+$("form").submit(function(){
   var p = profile;
   var msg = "<strong> " + user + "</strong>" + ": " + $("#m").val();
-  socket.emit('message', room, msg, p);
+  socket.emit("message", room, msg, p);
 
-  $('#m').val('');
+  $("#m").val("");
   return false;
 });
 
-socket.on('connect', function() {
-   socket.emit('room', room);
-   $('#messages').append($('<li>').text("You have entered the room, " + room));
+socket.on("connect", function() {
+   setTimeout(function() {
+     socket.emit("room", room, user);
+   }, 2000);
+   $("#messages").append($("<li>").text("You have entered the room, " + room));
 });
 
-socket.on('user connected', function(msg) {
-   $('#messages').append($('<li>').text(msg));
+socket.on("user connected", function(msg) {
+   $("#messages").append($("<li>").text(msg));
+});
+socket.on("user disconnected", function(msg) {
+   $("#messages").append($("<li>").text(msg));
 });
 
-socket.on('message', function(msg, profile){
+socket.on("message", function(msg, profile){
   var html_url = profile != undefined ? profile.html_url : "#";
   var thumbnail = profile != undefined ? profile.thumbnail : "https://avatars0.githubusercontent.com/u/1746301";
 
@@ -47,15 +66,16 @@ socket.on('message', function(msg, profile){
         })
       })
     }).append(msg));
+  emojify.run();
 
-   console.log('Incoming message:', msg);
+   console.log("Incoming message: ", msg);
 });
 
 // Hello.js
-hello.on('auth.login', function(r){
+hello.on("auth.login", function(r){
   // Get Profile
   console.log("auth.login");
-  hello.api(r.network+':/me', function(p){
+  hello.api(r.network+":/me", function(p){
     var tmpl = "<img src='"+ p.thumbnail + "' width=20/> " +
     "<strong>" + p.name + "</strong>";
     $("#github").html(tmpl);
@@ -69,4 +89,4 @@ hello.on('auth.login', function(r){
 
 hello.init({
   github : GITHUB_CLIENT_ID
-},	{redirect_uri:'../redirect.html', oauth_proxy:OAUTH_PROXY_URL});
+},	{redirect_uri:"../redirect.html", oauth_proxy:OAUTH_PROXY_URL});
